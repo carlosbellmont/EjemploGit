7ecdd4f8be87ae96c7362b70271234b953a1d647
Carlos de Tena Bellmont <carlostena@centroafuera.es>
2020-05-07 21:07:29 +0200

    Commit en la rama MasterMejora1

--- src/com/dam1t/Main.java
+++ src/com/dam1t/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hola DAM1T ¿Que tal?");
+        System.out.println("Hola DAM1T ¿Que tal? - ¿Como vas?");
         System.out.println("Ahora estoy en master");
+        System.out.println("Ahora estoy en master mejorado");
 
     }
 }
